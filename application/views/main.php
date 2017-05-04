6f7da0f7f29f93b1d27439d864eacebe897bbaec
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2017-05-04 11:06:31 +0700

    Add Timemap

--- application/views/main.php
+++ application/views/main.php
@@ -15,6 +15,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     <link rel="stylesheet" type="text/css" href="/assets/plugins/fullpagejs/jquery.fullpage.css" />
     <link rel="stylesheet" type="text/css" href="/assets/css/main.css" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
     <!--[if IE]>
     <script type="text/javascript">
@@ -30,8 +31,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <script type="text/javascript">
         $(document).ready(function() {
             $('#fullpage').fullpage({
-                sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-                anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+                sectionsColor: ['#999999', '#999999', '#999999', '#999999'],
+                anchors: ['intro', 'timeline', 'statistics', 'final'],
                 menu: '#menu',
                 navigation: true,
                 navigationPosition: 'right',
@@ -43,36 +44,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <ul id="menu">
-    <li data-menuanchor="firstPage" class="active"><a href="#firstPage">First slide</a></li>
-    <li data-menuanchor="secondPage"><a href="#secondPage">Second slide</a></li>
-    <li data-menuanchor="3rdPage"><a href="#3rdPage">Third slide</a></li>
+    <li data-menuanchor="firstPage" class="active"><a href="#intro">INTRO</a></li>
+    <li data-menuanchor="secondPage"><a href="#timeline">TIMELINE</a></li>
+    <li data-menuanchor="3rdPage"><a href="#statistics">STATISTICS</a></li>
+    <li data-menuanchor="3rdPage"><a href="#final">FINAL</a></li>
 </ul>
 
 
 <div id="fullpage">
-    <div class="section " id="section0">
+    <div class="section " id="intro-wrapper">
         <h1>Visdat</h1>
         <p>project</p>
     </div>
-    <div class="section active" id="section1">
-        <div class="slide" id="slide1">
-            <div class="intro">
-                <h1>Around the world scrolling</h1>
-                <p>Go to the first section and scroll up or to the last one and scroll down to see how it works.</p>
-            </div>
-        </div>
-        <div class="slide active" id="slide2">
-            <h1>Slide 2</h1>
-        </div>
-        <div class="slide" id="slide3">
-            <h1>Slide 3</h1>
-        </div>
+    <div class="section active" id="timeline-wrapper">
+        <iframe src="http://timemapper.okfnlabs.org/muhamadfikria2/world-war-2-data?embed=1" frameborder="0" style="border: none;" width="100%" height="100%"></iframe>
+        <a href="#statistics" class="next-button"><i class="fa fa-2x fa-chevron-down" aria-hidden="true"></i>  Go to Statistics</a>
+    </div>
+    <div class="section" id="statistics-wrapper">
 
     </div>
-    <div class="section" id="section2">
-        <div class="intro">
-            <h1>Slide 3</h1>
-        </div>
+    <div class="section" id="final-wrapper">
+
     </div>
 </div>
 
