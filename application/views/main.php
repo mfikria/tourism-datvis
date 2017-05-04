4526abe17b6f7ff561beaa8bbdefdd2c3a282e6f
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2017-05-04 20:16:09 +0700

    Create intro and final section

--- application/views/main.php
+++ application/views/main.php
@@ -6,16 +6,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Continuous scrolling - fullPage.js</title>
-    <meta name="author" content="Matthew Howell" />
-    <meta name="description" content="fullPage continuous scrolling demo." />
-    <meta name="keywords"  content="fullpage,jquery,demo,scroll,loop,continuous" />
+    <title>WORLD WAR 2 IN DATA</title>
+    <meta name="author" content="Muhamad Fikri Alhawarizmi" />
+    <meta name="description" content="Statistic World War 2" />
+    <meta name="keywords"  content="WW2, WORLD WAR 2" />
     <meta name="Resource-type" content="Document" />
 
 
     <link rel="stylesheet" type="text/css" href="/assets/plugins/fullpagejs/jquery.fullpage.css" />
     <link rel="stylesheet" type="text/css" href="/assets/css/main.css" />
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
 
     <!--[if IE]>
     <script type="text/javascript">
@@ -31,11 +32,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <script type="text/javascript">
         $(document).ready(function() {
             $('#fullpage').fullpage({
-                sectionsColor: ['#999999', '#999999', '#999999', '#999999'],
+                sectionsColor: ['transparent', '#999999', '#999999', '#999999'],
                 anchors: ['intro', 'timeline', 'statistics', 'final'],
                 menu: '#menu',
                 navigation: true,
                 navigationPosition: 'right',
+                afterLoad: function(anchorLink, index){
+                    var loadedSection = $(this);
+
+                    //using index
+                    if(index == 1){
+                        var vid = document.getElementById("bgvid");
+                        vid.play();
+                    }
+                }
             });
         });
     </script>
@@ -53,18 +63,41 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <div id="fullpage">
     <div class="section " id="intro-wrapper">
-        <h1>Visdat</h1>
-        <p>project</p>
+        <div class="bgvid-wrapper">
+            <video class="video" id="bgvid" playsinline autoplay muted loop>
+                <source src="/assets/video/intro.webm" type="video/webm">
+                <source src="/assets/video/intro.3gp" type="video/3gp"></video><!-- /video -->
+            <div id="intro-desc">
+                <h1>WW2</h1>
+                <p>
+                    The Second World War was arguably the most significant period of the 20th century. It brought about major leaps in technology and laid the groundwork that permitted post-war social changes including the end of European colonialism, the civil rights movement in the United States, and the modern women’s rights movement, as well as the programs for exploring outer space. The primary combatants were the Axis nations (Nazi Germany, Facist Italy, Imperial Japan and their smaller allies) and the Allied nations, led by Britain (and its Commonwealth nations), the Union of Soviet Socialist Republics and the United States of America. The Allies were the victors. Two superpowers, the USA and USSR, emerged from World War II to begin a Cold War with each other that would define much of the rest of the century.
+                </p>
+            </div>
+        </div>
+
     </div>
     <div class="section active" id="timeline-wrapper">
         <iframe src="http://timemapper.okfnlabs.org/muhamadfikria2/world-war-2-data?embed=1" frameborder="0" style="border: none;" width="100%" height="100%"></iframe>
         <a href="#statistics" class="next-button"><i class="fa fa-2x fa-chevron-down" aria-hidden="true"></i>  Go to Statistics</a>
     </div>
     <div class="section" id="statistics-wrapper">
-
+        <h1>
+            Test
+        </h1>
     </div>
     <div class="section" id="final-wrapper">
-
+        <p class="quote">
+            War does not <br>determine who is
+        </p>
+        <h1>
+            RIGHT
+        </h1>
+        <p class="quote">
+            only who is <span style="color: red"> left</span>.
+        </p>
+        <p class="quote-author">
+            ─ Bertrand Russell
+        </p>
     </div>
 </div>
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,22 @@ $(document).ready(function(){
 	$("#demosMenu").change(function(){
 	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
 	});
+
+    (function() {
+        /**
+         * Video element
+         * @type {HTMLElement}
+         */
+        var video = document.getElementById("bgvid");
+
+        /**
+         * Check if video can play, and play it
+         */
+        video.addEventListener( "canplay", function() {
+            video.play();
+        });
+    })();
+
 });
+
+
