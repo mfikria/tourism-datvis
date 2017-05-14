827fe7a1ffb13e97ed7abfbcb9dc8f239a303137
ryanyonata <ryan.yonata@gmail.com>
2017-05-14 18:35:09 +0700

    Moved js file for treemap

--- application/views/main.php
+++ application/views/main.php
@@ -13,8 +13,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta name="Resource-type" content="Document" />
 
 
-    <link rel="stylesheet" type="text/css" href="assets/plugins/fullpagejs/jquery.fullpage.css" />
-    <link rel="stylesheet" type="text/css" href="assets/css/main.css" />
+    <link rel="stylesheet" type="text/css" href="/assets/plugins/fullpagejs/jquery.fullpage.css" />
+    <link rel="stylesheet" type="text/css" href="/assets/css/main.css" />
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
 
@@ -27,9 +27,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
 
-    <script type="text/javascript" src="assets/plugins/fullpagejs/jquery.fullpage.js"></script>
+    <script type="text/javascript" src="/assets/plugins/fullpagejs/jquery.fullpage.js"></script>
     <script src="https://d3js.org/d3.v4.min.js"></script>
-    <script type="text/javascript" src="assets/js/main.js"></script>
+    <script type="text/javascript" src="/assets/js/main.js"></script>
     <script type="text/javascript">
         $(document).ready(function() {
             $('#fullpage').fullpage({
@@ -65,8 +65,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div class="section " id="intro-wrapper">
         <div class="bgvid-wrapper">
             <video class="video" id="bgvid" playsinline autoplay muted loop>
-                <source src="assets/video/intro.webm" type="video/webm">
-                <source src="assets/video/intro.3gp" type="video/3gp"></video><!-- /video -->
+                <source src="/assets/video/intro.webm" type="video/webm">
+                <source src="/assets/video/intro.3gp" type="video/3gp"></video><!-- /video -->
             <div id="intro-desc">
                 <h1>WW2</h1>
                 <p>
@@ -102,92 +102,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </p>
     </div>
 </div>
-
-<script type="text/javascript">
-    var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
-
-    var fader = function(color) { return d3.interpolateRgb(color, "#fff")(0.2); },
-        color = d3.scaleOrdinal(d3.schemeCategory20.map(fader)),
-        format = d3.format(",d");
-
-    var treemap = d3.treemap()
-        .tile(d3.treemapResquarify)
-        .size([width, height])
-        .round(true)
-        .paddingInner(1);
-
-    d3.json("assets/json/casualties.json", function(error, data) {
-      if (error) throw error;
-
-      var root = d3.hierarchy(data)
-          .eachBefore(function(d) { d.data.id = (d.parent ? d.parent.data.id + "." : "") + d.data.name; })
-          .sum(sumBySize)
-          .sort(function(a, b) { return b.height - a.height || b.value - a.value; });
-
-      treemap(root);
-
-      var cell = svg.selectAll("g")
-        .data(root.leaves())
-        .enter().append("g")
-          .attr("transform", function(d) { return "translate(" + d.x0 + "," + d.y0 + ")"; });
-
-      cell.append("rect")
-          .attr("id", function(d) { return d.data.id; })
-          .attr("width", function(d) { return d.x1 - d.x0; })
-          .attr("height", function(d) { return d.y1 - d.y0; })
-          .attr("fill", function(d) { return color(d.parent.data.id); });
-
-      cell.append("clipPath")
-          .attr("id", function(d) { return "clip-" + d.data.id; })
-        .append("use")
-          .attr("xlink:href", function(d) { return "#" + d.data.id; });
-
-      cell.append("text")
-          .attr("clip-path", function(d) { return "url(#clip-" + d.data.id + ")"; })
-        .selectAll("tspan")
-          .data(function(d) { return d.data.name.split(/(?=[A-Z][^A-Z])/g); })
-        .enter().append("tspan")
-          .attr("x", 4)
-          .attr("y", function(d, i) { return 13 + i * 10; })
-          .text(function(d) { return d; });
-
-      cell.append("title")
-          .text(function(d) { return d.data.id + "\n" + format(d.value); });
-
-      d3.selectAll("input")
-          .data([sumBySize, sumByCount], function(d) { return d ? d.name : this.value; })
-          .on("change", changed);
-
-      var timeout = d3.timeout(function() {
-        d3.select("input[value=\"sumByCount\"]")
-            .property("checked", true)
-            .dispatch("change");
-      }, 2000);
-
-      function changed(sum) {
-        timeout.stop();
-
-        treemap(root.sum(sum));
-
-        cell.transition()
-            .duration(750)
-            .attr("transform", function(d) { return "translate(" + d.x0 + "," + d.y0 + ")"; })
-          .select("rect")
-            .attr("width", function(d) { return d.x1 - d.x0; })
-            .attr("height", function(d) { return d.y1 - d.y0; });
-      }
-    });
-
-    function sumByCount(d) {
-      return d.children ? 0 : 1;
-    }
-
-    function sumBySize(d) {
-      return d.size;
-    }
-  </script>
+<script type="text/javascript" src="/assets/js/treemap.js"></script>
 
 </body>
 </html>
